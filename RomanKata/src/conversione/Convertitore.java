51cfb99aa7004cff5b673b6d9ae9c5b787131edc
aleric <ale.ricchiuti@hotmail.it>
2015-03-08 21:27:54 +0100

    Ottimizzazione codice.

--- RomanKata/src/conversione/Convertitore.java
+++ RomanKata/src/conversione/Convertitore.java
@@ -1,45 +1,46 @@
 package conversione;
+
 public class Convertitore {
 
 	public String converti(String numeroRomano) {
 
+		char[] cifreNumeroRomano = numeroRomano.toCharArray();
+		return String.valueOf(calcolaNumeroRomano(cifreNumeroRomano));
+	}
+
+	private int calcolaNumeroRomano(char[] cifre) {
+
 		int summa = 0;
+		int i = 0;
+		for (; i < cifre.length - 1; i += 2) {
 
-		String[] split = numeroRomano.split("");
-
-		int i = 1;
-		while (i < split.length - 1) {
-
-			int valoreCifra = convertiCifra(split[i].toUpperCase());
-			int valoreCifraSuccessiva = convertiCifra(split[i + 1]
-					.toUpperCase());
+			int valoreCifra = convertiCifra(cifre[i]);
+			int valoreCifraSuccessiva = convertiCifra(cifre[i + 1]);
 
 			if (valoreCifra < valoreCifraSuccessiva) {
 				summa += valoreCifraSuccessiva - valoreCifra;
 			} else {
 				summa += valoreCifra + valoreCifraSuccessiva;
 			}
-			i += 2;
 		}
 		/* se necessario converti l'ultima cifra */
-		if (i < split.length) {
-			summa += convertiCifra(split[split.length - 1]);
+		if (i < cifre.length) {
+			summa += convertiCifra(cifre[cifre.length - 1]);
 		}
-
-		return String.valueOf(summa);
+		return summa;
 	}
 
-	private int convertiCifra(String cifra) {
+	private int convertiCifra(char cifra) {
 
-		if (cifra.equals("M")) {
+		if (cifra == 'M' || cifra == 'm') {
 			return 1000;
-		} else if (cifra.equals("C")) {
+		} else if (cifra == 'C' || cifra == 'c') {
 			return 100;
-		} else if (cifra.equals("L")) {
+		} else if (cifra == 'L' || cifra == 'l') {
 			return 50;
-		} else if (cifra.equals("X")) {
+		} else if (cifra == 'X' || cifra == 'x') {
 			return 10;
-		} else if (cifra.equals("V")) {
+		} else if (cifra == 'V' || cifra == 'v') {
 			return 5;
 		} else {
 			return 1;
