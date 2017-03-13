0e7edab717149a088c73b625b16190ec75d7fd4c
Alessandro Ricchiuti <ale.ricchiuti@hotmail.it>
2017-03-13 11:51:51 +0100

    Refactoring.

--- RomanKata/src/conversione/Convertitore.java
+++ RomanKata/src/conversione/Convertitore.java
@@ -2,51 +2,51 @@ package conversione;
 
 public class Convertitore {
 
-	public String converti(String numeroRomano) {
+    public String converti(String numeroRomano) {
 
-		char[] cifreNumeroRomano = numeroRomano.toUpperCase().toCharArray();
-		return String.valueOf(convertiCifreNumeroRomano(cifreNumeroRomano));
-	}
+        char[] cifreNumeroRomano = numeroRomano.toUpperCase().toCharArray();
+        return String.valueOf(convertiCifreNumeroRomano(cifreNumeroRomano));
+    }
 
-	private int convertiCifreNumeroRomano(char[] cifre) {
+    private int convertiCifreNumeroRomano(char[] cifre) {
 
-		int summa = 0;
-		int i = 0;
-		for (; i < cifre.length - 1; i += 2) {
+        int summa = 0;
+        int i = 0;
+        for (; i < cifre.length - 1; i += 2) {
 
-			int valoreCifra = convertiCifra(cifre[i]);
-			int valoreCifraSuccessiva = convertiCifra(cifre[i + 1]);
+            int valoreCifra = convertiCifra(cifre[i]);
+            int valoreCifraSuccessiva = convertiCifra(cifre[i + 1]);
 
-			if (valoreCifra < valoreCifraSuccessiva) {
-				summa += valoreCifraSuccessiva - valoreCifra;
-			} else {
-				summa += valoreCifra + valoreCifraSuccessiva;
-			}
-		}
-		/* se necessario converti l'ultima cifra */
-		if (i < cifre.length) {
-			summa += convertiCifra(cifre[cifre.length - 1]);
-		}
-		return summa;
-	}
+            if (valoreCifra < valoreCifraSuccessiva) {
+                summa += valoreCifraSuccessiva - valoreCifra;
+            } else {
+                summa += valoreCifra + valoreCifraSuccessiva;
+            }
+        }
+        /* se necessario converti l'ultima cifra */
+        if (i < cifre.length) {
+            summa += convertiCifra(cifre[cifre.length - 1]);
+        }
+        return summa;
+    }
 
-	private int convertiCifra(char cifra) {
+    private int convertiCifra(char cifra) {
 
-		if (cifra == 'M') {
-			return 1000;
-		} else if (cifra == 'C') {
-			return 100;
-		} else if (cifra == 'L') {
-			return 50;
-		} else if (cifra == 'X') {
-			return 10;
-		} else if (cifra == 'V') {
-			return 5;
-		} else if (cifra == 'I') {
-			return 1;
-		} else {
-			throw new IllegalArgumentException("'" + cifra
-					+ "' non è un numero romano");
-		}
-	}
+        switch (cifra) {
+            case 'M':
+                return 1000;
+            case 'C':
+                return 100;
+            case 'L':
+                return 50;
+            case 'X':
+                return 10;
+            case 'V':
+                return 5;
+            case 'I':
+                return 1;
+            default:
+                throw new IllegalArgumentException("'" + cifra + "' non è un numero romano");
+        }
+    }
 }
